5387c4caf362a75b62fcaf7eedb50e854a286a36
nanabilaa <putriiynabila@gmail.com>
2024-08-24 20:16:35 +0700

    commit satu

---
+++ Timeline.js
@@ -0,0 +1,9 @@
+// Simple smooth scroll to section on link click
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
